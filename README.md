7caef7562c610332a195122f6fa4961566033f46
Hoàng Phúc <lhphuc1602@gmail.com>
2023-05-05 01:43:34 +0700

    Đô án PTUD Web

---
+++ README.md
@@ -0,0 +1,25 @@
+## How to run
+  - Open 2 terminal
+
+## First terminal
+```sh
+cd .\backend\
+
+npm install
+
+npm start
+```
+Server run in:
+www.localhost:3000
+
+## Second terminal
+```sh
+cd .\frontend\
+
+npm install
+
+npm run dev
+```
+Client run in:
+www.localhost:3001
+
---
+++ backend/app/config/index.js
@@ -0,0 +1,12 @@
+const config = {
+	app: {
+		port: process.env.PORT || 3000,
+	},
+	db: {
+		uri: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/learning-management",
+	},
+	jwt: {
+		secret: process.env.JWT_SECRET || "project-secret-key",
+	},
+};
+module.exports = config;
